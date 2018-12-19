30906dc7337697eb78c10783ca31c2d5ee6078a7
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2018-12-19 21:43:49 +0900

    Finished the implementation of the update step

--- include/state-observation/dynamics-estimators/kinetics-observer.hpp
+++ include/state-observation/dynamics-estimators/kinetics-observer.hpp
@@ -36,9 +36,16 @@ static const int stateSizeBase = 3+4+3+3+3+3+3;
 static const int stateSizePerContact = 3+4+3+3;
 static const int stateTangentSizeBase = 3+3+3+3+3+3+3;
 static const int stateTangentSizePerContact = 3+3+3+3;
-static const int signalSizeIMU = 6;
-static const int signalSizeFTSensor = 6;
-static const int signalSizePoseSensor = 7;
+
+static const double defaultdx=1e-6;///default derivation steps
+
+static const int sizeAcceleroSignal = 3;
+static const int sizeGyroSignal = 3;
+static const int sizeIMUSignal = sizeAcceleroSignal+sizeGyroSignal;
+
+static const int sizeFTSignal = 6;
+static const int sizePoseSignal = 7;
+static const int sizePoseSignalTangent = 6;
 
 
 
@@ -59,20 +66,21 @@ namespace stateObservation
     public:
         typedef kine::Kinematics Kinematics;
 
-        ///virtual destructor
-        virtual ~KineticsObserver();
-
         /// The constructor.
         ///  \li maxContacts : maximum number of contacts,
         ///  \li dx gives the derivation step for a finite differences derivation method
         KineticsObserver(int maxContacts=4);
+
+
+        ///virtual destructor
+        virtual ~KineticsObserver();
+
 
         ///Gets the state size
         virtual unsigned getStateSize() const;
 
         ///Gets the measurement size
         virtual unsigned getMeasurementSize() const;
-
 
         /// ///////////////////////////////////////////////////////////
         /// Getting, setting the current time and running the estimation
@@ -92,7 +100,7 @@ namespace stateObservation
         /// ////////////////////////////////
 
         /// Gets an estimation of the state in the form of a state vector $\hat{x_{k+1}}$
-        virtual Vector getState() const;
+        virtual Vector getStateVector() const;
 
         /// Get the Kinematics of the observed frams
         virtual Kinematics getKinematics() const;
@@ -101,7 +109,7 @@ namespace stateObservation
         virtual Kinematics getKinematics(const Kinematics &) const;
 
         ///get the contact force provided by the estimator 
-        /// different from a contact sensor measurement
+        /// which is different from a contact sensor measurement
         virtual Vector3 getContactForce(int contactNbr);
         virtual Vector3 getContactMoment(int contactNbr);
         virtual Vector6 getContactForceandMoment(int contactNbr);
@@ -116,9 +124,10 @@ namespace stateObservation
         virtual void estimateAcceleration();
 
         ///Sets a value of the state x_k provided from another source
-        /// can be used for initialization of the estimator
-        ///can be a 4x4 homogenous matrix or a vector of size stateSize
-        virtual void setState(const Matrix &, bool resetCovariance=true);
+        /// can be used for initialization of the estimator or use 
+        /// setStateVector
+        virtual void setStateVector(Vector3,bool resetCovariance=true);
+
 
         ///Sets a value for the kinematics part of the state
         virtual void setKinematics(const Kinematics &, bool resetForces=true,
@@ -148,7 +157,7 @@ namespace stateObservation
                                                         const Matrix3 gyroCov, const Kinematics &localKine, int num=-1);
         
 
-
+        
         virtual void setIMUDefaultCovarianceMatrix(const Matrix3& acceleroCov, const Matrix3 gyroCov);
 
 
@@ -161,13 +170,14 @@ namespace stateObservation
         /// the best is to provide the position , the orientation, 
         /// the angular and the linear velocities. 
         virtual void setContactFTSensor(const Vector3& force, const Vector3& torque, const Kinematics &localKine, int contactNumber);
-        virtual void setContactFTSensor(const Vector3& force, const Vector3& torque, const Matrix6 ForcetorqueCovMatrix, const Kinematics &localKine, int contactNumber);
+        virtual void setContactFTSensor(const Vector3& force, const Vector3& torque, const Matrix6 ForcetorqueCovMatrix, 
+                                                                                    const Kinematics &localKine, int contactNumber);
 
         virtual void setContactFTSensorDefaultCovarianceMatrix(const Matrix6 & forceTorqueSensorCovMat);
         
         /// sets the the kinematics of the contact expressed in the observed frame
         /// the best is to provide the position, the orientation, the angular and the linear velocities. 
-        virtual void setContactWithoutSensor(const Kinematics &localKine, int contactNumber);
+        virtual void setContactWithNoSensor(const Kinematics &localKine, int contactNumber);
                                                                
         /// Set a measurement of the pose. The input is the Measured kinematics 
         /// namely position and orientation.
@@ -212,6 +222,7 @@ namespace stateObservation
 
         std::vector<int> getListOfContact() const;
 
+        
 
         ///Sets the covariance matrix of the flexibility Guess
         virtual void setStateCovariance(const Matrix & P);
@@ -237,7 +248,6 @@ namespace stateObservation
         virtual void resetCovarianceMatrices();
 
         ///to reset all the sensor inputs and provided contact positions
-        ///does NOT reset the presence of contacts - call clearContacts for that instead
         void resetIteration();
 
 
@@ -283,37 +293,14 @@ protected:
 
     protected:
         
-               
-
-        Matrix3 acceleroDefaultCovMat_;
-        Matrix3 gyroCovDefaultMat_;
-        Matrix6 contactFTSensorCovMat_;
-        Matrix6 poseSensorCovMat_;
-
-
-
-        ///function to call before all the measurements
-        ///detects if there is a new estimation beginning and then
-        ///calls the reset of the iteration
-        virtual void onSetMeasurement_();
-        
-        stateObservation::ExtendedKalmanFilter ekf_;
-
-        bool finiteDifferencesJacobians_;
-
-        Vector dx_;
-
-        Vector lastX_;
-
-        TimeIndex k_est;
-        TimeIndex k_input;
-
         struct Sensor
         {
+            Sensor(int signalSize):size(signalSize), time(0) {}
             virtual ~Sensor(){}
-            int size;
             int index;
-
+            int size;
+            int time;
+            
             inline Vector extractFromVector(const Vector & v){return v.segment(size,index);}
         };
 
@@ -321,41 +308,90 @@ protected:
         public Sensor
         {
             virtual ~IMU(){}
+            IMU():Sensor(sizeIMUSignal){}
             Kinematics kinematics;
-            Vector3 accelero;
-            Vector3 gyro;
-            Matrix3 acceleroCov;
-            Matrix3 gyroCov;
+            Vector6 acceleroGyro;
+            Matrix3 covMatrixAccelero;
+            Matrix3 covMatrixGyro;
             EIGEN_MAKE_ALIGNED_OPERATOR_NEW        
         };
 
         typedef std::map<int, IMU, std::less<int>, 
              Eigen::aligned_allocator<std::pair<const int, IMU> > > MapIMU;
+        typedef MapIMU::iterator MapIMUIterator;
         typedef std::pair<int, IMU> PairIMU;
 
-        MapIMU mapIMU_;
-
-        struct ContactFTSensor:
+        
+        struct Contact:
         public Sensor
         {
-            virtual ~ContactFTSensor(){}
+            Contact():Sensor(sizeFTSignal),withRealSensor(false){}
+            virtual ~Contact(){}
             Kinematics kinematics;
-            Vector3 force;
-            Vector3 torque;
-            Matrix6 cov;
-            bool withSensor;
+            Vector6 forceTorque;
+            Matrix6 covMatrix;
+            bool withRealSensor;
 
-            ContactFTSensor():withSensor(false){}
+            static int numberOfRealSensors;
+
+
             EIGEN_MAKE_ALIGNED_OPERATOR_NEW        
         };
+
+        struct dynamicsContainers
+        {
+            Kinematics state;
+
+            EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        }
+
+
     
-        typedef std::map<int, ContactFTSensor, std::less<int>, 
-             Eigen::aligned_allocator<std::pair<const int, IMU> > > MapFTSensor;
-        typedef std::pair<int, IMU> PairFTSensor;
+        typedef std::map<int, Contact, std::less<int>, 
+             Eigen::aligned_allocator<std::pair<const int, Contact> > > MapContact;
+        typedef MapContact::iterator MapContactIterator;
+        typedef std::pair<int, Contact> PairContact;
 
-        MapFTSensor mapFT_;
+        struct AbsolutePoseSensor:
+        public Sensor
+        {
+            AbsolutePoseSensor():Sensor(sizePoseSignal),isSet(false){}
 
-            std::set<int> contacts;
+            Kinematics pose;
+            Matrix6 covMatrix;
+            bool isSet;
+        };
+
+        AbsolutePoseSensor absPoseSensor_;
+        MapIMU imuSensors_;
+        MapContact contacts_;
+
+        int stateSize_; 
+        int stateTangentSize_;
+        int measurementSize_; 
+
+        Vector stateVector_;
+        Vector stateVectorDx_;
+        Vector oldStateVector_; 
+
+        Vector measurementVector_;
+        Matrix measurementCovMatrix_;    
+
+        Matrix3 acceleroDefaultCovMat_;
+        Matrix3 gyroCovDefaultMat_;
+        Matrix6 contactFTSensorDefaultCovMat_;
+        Matrix6 poseSensorCovMat_;
+        bool finiteDifferencesJacobians_;
+
+        stateObservation::ExtendedKalmanFilter ekf_;
+
+        TimeIndex k_est;
+        TimeIndex k_data;
+
+        ///function to call before all the measurements
+        ///detects if there is a new estimation beginning and then
+        ///calls the reset of the iteration
+        virtual void startNewIteration_();
 
     private:
 
@@ -365,4 +401,5 @@ protected:
     };
 
 }
+
 #endif // FLEXIBILITYESTIMATION_EKFFLEXIBILITYESTIMATORBASE_H
