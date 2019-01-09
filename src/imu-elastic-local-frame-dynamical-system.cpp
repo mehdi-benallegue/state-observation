63d6b23705fc52f93a8e53f569260d8ef238b28c
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-01-09 19:57:58 +0900

    Initialize op_.positionComBias before addding it to the center of mass position

--- src/imu-elastic-local-frame-dynamical-system.cpp
+++ src/imu-elastic-local-frame-dynamical-system.cpp
@@ -943,7 +943,14 @@ namespace stateObservation
         op_.contactOriV.setValue(u.segment<3>(input::contacts +12*i+3),i);
       }
       op_.positionCom=u.segment<3>(input::posCom);
-      if(withComBias_) op_.positionCom-=op_.positionComBias;
+      
+      if(withComBias_)
+      {
+        op_.positionComBias <<  x.segment(state::comBias,2),
+                              0;// the bias of the com along the z axis is assumed 0.
+        op_.positionCom-=op_.positionComBias;
+      }
+
       op_.velocityCom=u.segment<3>(input::velCom);
       op_.accelerationCom=u.segment<3>(input::accCom);
       op_.AngMomentum=u.segment<3>(input::angMoment);
