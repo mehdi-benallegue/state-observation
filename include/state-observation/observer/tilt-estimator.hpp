ee1669166629d002d3b1c292e5ce0d60f7912d2f
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-01-29 17:07:23 +0900

    Add documentaion and  aligned new operator for eigen types, optimized variables

--- include/state-observation/observer/tilt-estimator.hpp
+++ include/state-observation/observer/tilt-estimator.hpp
@@ -25,7 +25,9 @@ namespace stateObservation
   * \brief
   *         Description is pending
   *
-  *
+  *         use getEstimatedState to obtain the state vector
+  *         the tilt R.transpose()*e_z is constituted
+  *         with the last three components of the state vector.
   *
   */
   class TiltEstimator: public ZeroDelayObserver
@@ -39,34 +41,46 @@ namespace stateObservation
     ///  \li gamma : parameter related to the orthogonality
     TiltEstimator(double alpha, double beta, double gamma);
 
+    ///set the gain of x1_hat variable
     void setAlpha(const double alpha) { alpha_ = alpha; }
     double getAlpha() const { return alpha_; }
     
+    ///set the gain of x2prime_hat variable
     void setBeta(const double beta) { beta_ = beta; }
     double getBeta() const { return beta_; }
 
+    ///set the gain of x2_hat variable
     void setGamma(const double gamma) { gamma_ = gamma; }
     double getGamma() const { return gamma_; }
     
+    ///set the sampling time of the measurements
     void setSamplingTime(const double dt) { dt_ = dt; }
     double getSamplingTime() const { return dt_; }
 
-    void setSensorPositionInC(const Vector3& p) { p_S_C = p; }
-    Vector3 getSensorPositionInC() { return p_S_C; }
+    /// sets the position of the IMU sensor in the control frame
+    void setSensorPositionInC(const Vector3& p) { p_S_C_ = p; }
+    Vector3 getSensorPositionInC() { return p_S_C_; }
     
-    void setSensorOrientationInC(const Matrix3& R) { R_S_C = R; }
-    Matrix3 getSensorOrientationInC() { return R_S_C; }
+    /// sets the oriantation of the IMU sensor in the control frame
+    void setSensorOrientationInC(const Matrix3& R) { R_S_C_ = R; }
+    Matrix3 getSensorOrientationInC() { return R_S_C_; }
     
-    void setSensorLinearVelocityInC(const Vector3& v) { v_S_C = v; }
-    Vector3 getSensorLinearVelocityInC() { return v_S_C; }
+    /// sets teh linear velocity of the IMU sensor in the control frame
+    void setSensorLinearVelocityInC(const Vector3& v) { v_S_C_ = v; }
+    Vector3 getSensorLinearVelocityInC() { return v_S_C_; }
     
-    void setSensorAngularVelocityInC(const Vector3& w) { w_S_C = w; }
-    Vector3 getSensorAngularVelocityInC() { return w_S_C; }
+    /// sets the angular velocity of the IMU sensor in the control frame
+    void setSensorAngularVelocityInC(const Vector3& w) { w_S_C_ = w; }
+    Vector3 getSensorAngularVelocityInC() { return w_S_C_; }
 
-    void setControlOriginVelocityInW(const Vector3& v) { v_C = v; }
-    Vector3 getControlOriginVelocityInW() { return v_C; }
+    /// sets the velocity of the control origin in the world frame
+    void setControlOriginVelocityInW(const Vector3& v) { v_C_ = v; }
+    Vector3 getControlOriginVelocityInW() { return v_C_; }
     
+    /// sets ths measurement (accelero and gyro stacked in one vector)
     void setMeasurement(const Vector3 ya_k, const Vector3 yg_k, TimeIndex k);
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     
   protected:
 
@@ -77,19 +91,30 @@ namespace stateObservation
     double dt_;
     
     /// Position of the IMU in the control frame
-    Vector3 p_S_C;
+    Vector3 p_S_C_;
 
     /// Orientation of the IMU in the control frame
-    Matrix3 R_S_C;
+    Matrix3 R_S_C_;
 
     /// Linear velocity of the IMU in the control frame
-    Vector3 v_S_C;
+    Vector3 v_S_C_;
 
     /// Angular velocity of the IMU in the control frame
-    Vector3 w_S_C;
+    Vector3 w_S_C_;
 
     /// Linear velocity of the control frame
-    Vector3 v_C;
+    Vector3 v_C_;
+
+
+    ///variables used for the computation
+    Vector3 x1_;
+    Vector3 x1_hat_;
+    Vector3 x2_hat_prime_;
+    Vector3 x2_hat_;
+    Vector3 dx1_hat;
+    
+    
+
     
     /// The tilt estimator loop
     StateVector oneStepEstimation_();
--- src/tilt-estimator.cpp
+++ src/tilt-estimator.cpp
@@ -4,8 +4,16 @@ namespace stateObservation
 {
 
   TiltEstimator::TiltEstimator(double alpha, double beta, double gamma)
-    : ZeroDelayObserver(9, 6), alpha_(alpha), beta_(beta), gamma_(gamma),
-      p_S_C(Vector3::Zero()), R_S_C(Matrix3::Identity()), v_S_C(Vector3::Zero()), w_S_C(Vector3::Zero()), v_C(Vector3::Zero())
+    : ZeroDelayObserver(9, 6), 
+      alpha_(alpha), 
+      beta_(beta), 
+      gamma_(gamma),
+      dt_(0.005),
+      p_S_C_(Vector3::Zero()),
+      R_S_C_(Matrix3::Identity()), 
+      v_S_C_(Vector3::Zero()), 
+      w_S_C_(Vector3::Zero()), 
+      v_C_(Vector3::Zero())
   {
   }
   
@@ -26,17 +34,17 @@ namespace stateObservation
     Vector3 ya = getMeasurement(k+1).head<3>();
     Vector3 yg = getMeasurement(k+1).tail<3>();
     
-    Vector3 x1 = R_S_C.transpose() * (v_C + v_S_C) + (yg - R_S_C.transpose() * w_S_C).cross(R_S_C.transpose() * p_S_C);
+    x1_ = R_S_C_.transpose() * (v_C_ + v_S_C_) + (yg - R_S_C_.transpose() * w_S_C_).cross(R_S_C_.transpose() * p_S_C_);
 
     ObserverBase::StateVector x_hat = getEstimatedState(k);
-    Vector3 x1_hat = x_hat.segment<3>(0);
-    Vector3 x2_hat_prime = x_hat.segment<3>(3);
-    Vector3 x2_hat = x_hat.segment<3>(6);
+    x1_hat_ = x_hat.segment<3>(0);
+    x2_hat_prime_ = x_hat.segment<3>(3);
+    x2_hat_ = x_hat.segment<3>(6);
     
     Vector dx_hat(9);
-    dx_hat.segment<3>(0) = x1_hat.cross(yg) - cst::gravityConstant * x2_hat_prime + ya + alpha_ * (x1 - x1_hat);
-    dx_hat.segment<3>(3) = x2_hat_prime.cross(yg) - beta_ * (x1 - x1_hat);
-    dx_hat.segment<3>(6) = x2_hat.cross(yg - gamma_ * x2_hat.cross(x2_hat_prime));
+    dx_hat.segment<3>(0) = x1_hat_.cross(yg) - cst::gravityConstant * x2_hat_prime_ + ya + alpha_ * (x1_ - x1_hat_);
+    dx_hat.segment<3>(3) = x2_hat_prime_.cross(yg) - beta_ * (x1_ - x1_hat_);
+    dx_hat.segment<3>(6) = x2_hat_.cross(yg - gamma_ * x2_hat_.cross(x2_hat_prime_));
     
     x_hat += dx_hat * dt_;
 
