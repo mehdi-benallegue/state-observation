1308afb17762d9eba515debc6fbb5a47cbefb6d3
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-12-19 20:41:52 +0900

    Add a test for Kinetics observer

---
+++ unit-testings/test-kinetics-observer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <state-observation/dynamics-estimators/kinetics-observer.hpp>
+
+using namespace stateObservation;
+int main()
+{
+    KineticsObserver o;
+
+    std::cout <<"successfully built" <<std::endl;
+
+    return 0;
+}
