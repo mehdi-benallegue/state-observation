12f6fd2516db115d6831f7430dd72f259573d682
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-12-24 19:21:46 +0900

    Add tangent version of index functions

--- include/state-observation/dynamics-estimators/kinetics-observer.hxx
+++ include/state-observation/dynamics-estimators/kinetics-observer.hxx
@@ -99,6 +99,106 @@ inline unsigned KineticsObserver::contactTorqueIndex(MapContactConstIterator i) 
 }
 
 
+inline unsigned KineticsObserver::kineIndexTangent() const
+{
+  return 0;
+}
+inline unsigned KineticsObserver::posIndexTangent() const
+{
+  return 0;
+}
+inline unsigned KineticsObserver::oriIndexTangent() const
+{
+  return posIndexTangent()+sizePos;
+}
+inline unsigned KineticsObserver::linVelIndexTangent() const
+{
+  return oriIndexTangent()+sizeOriTangent;
+}
+inline unsigned KineticsObserver::angVelIndexTangent() const
+{
+  return linVelIndexTangent()+sizeLinVel;
+}
+inline unsigned KineticsObserver::gyroBiasIndexTangent() const
+{
+  return angVelIndexTangent() + sizeAngVel;
+}
+inline unsigned KineticsObserver::unmodeledWrenchIndexTangent() const
+{
+  return gyroBiasIndexTangent()+sizeGyroBias;
+}
+inline unsigned KineticsObserver::unmodeledForceIndexTangent() const
+{
+  return unmodeledWrenchIndexTangent();
+}
+inline unsigned KineticsObserver::contactsIndexTangent() const
+{
+  return unmodeledWrenchIndexTangent()+sizeWrench;
+}
+inline unsigned KineticsObserver::unmodeledTorqueIndexTangent() const
+{
+  return unmodeledForceIndexTangent()+sizeForce;
+}
+inline unsigned KineticsObserver::contactIndexTangent(int contactNbr) const
+{
+    BOOST_ASSERT(contacts_.find(contactNbr)!=contacts_.end() && \
+                  "The requested contact is not set yet, please add it before");
+  return contacts_.find(contactNbr)->second.stateIndexTangent;
+}
+inline unsigned KineticsObserver::contactKineIndexTangent(int contactNbr) const
+{
+  return contactIndexTangent(contactNbr);
+}
+inline unsigned KineticsObserver::contactPosIndexTangent(int contactNbr) const
+{
+  return contactKineIndexTangent(contactNbr);
+}
+inline unsigned KineticsObserver::contactOriIndexTangent(int contactNbr) const
+{
+  return contactPosIndexTangent(contactNbr)+sizePos;
+}
+inline unsigned KineticsObserver::contactWrenchIndexTangent(int contactNbr) const
+{
+  return contactKineIndexTangent(contactNbr)+sizeContactKineTangent;
+}
+inline unsigned KineticsObserver::contactForceIndexTangent(int contactNbr) const
+{
+  return contactWrenchIndexTangent(contactNbr);
+}
+inline unsigned KineticsObserver::contactTorqueIndexTangent(int contactNbr) const
+{
+  return contactForceIndexTangent(contactNbr)+sizeForce;
+}
+
+inline unsigned KineticsObserver::contactIndexTangent(MapContactConstIterator i) const
+{  
+  return i->second.stateIndexTangent;
+}
+inline unsigned KineticsObserver::contactKineIndexTangent(MapContactConstIterator i) const
+{
+  return contactIndexTangent(i);
+}
+inline unsigned KineticsObserver::contactPosIndexTangent(MapContactConstIterator i) const
+{
+  return contactKineIndexTangent(i);
+}
+inline unsigned KineticsObserver::contactOriIndexTangent(MapContactConstIterator i) const
+{
+  return contactPosIndexTangent(i)+sizePos;
+}
+inline unsigned KineticsObserver::contactWrenchIndexTangent(MapContactConstIterator i) const
+{
+  return contactKineIndexTangent(i)+sizeContactKineTangent;
+}
+inline unsigned KineticsObserver::contactForceIndexTangent(MapContactConstIterator i) const
+{
+  return contactWrenchIndexTangent(i);
+}
+inline unsigned KineticsObserver::contactTorqueIndexTangent(MapContactConstIterator i) const
+{
+  return contactForceIndexTangent(i)+sizeForce;
+}
+
 
 
 
