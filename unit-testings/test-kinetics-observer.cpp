31b6cd5b479b8ccafe0698a8381feb199b68a9d5
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2020-01-21 21:15:59 +0900

    Update the test to take the new representation into account

--- unit-testings/test-kinetics-observer.cpp
+++ unit-testings/test-kinetics-observer.cpp
@@ -561,10 +561,10 @@ int testKinematics (int errcode)
 
 }
 
-int testKineticsObserver(int code)
+int testKineticsObserverCodeAccessor(int code)
 {
     double dt = 0.001;
-    KineticsObserver o(6);
+    KineticsObserver o(4,2);
 
     Vector x0(o.getStateSize());
     x0.setZero();
@@ -575,16 +575,75 @@ int testKineticsObserver(int code)
 
     Kinematics stateKine;
 
-    stateKine.position.set() << 0,0,0.7;
-    stateKine.orientation.setZeroRotation();
+    stateKine.position.set() << 0.1,0,0.7;
+    stateKine.orientation =  Vector3(0,0,0);
     stateKine.linVel.set().setZero();
     stateKine.angVel.set().setZero();
 
 
+
+    
+
+    o.setStateVector(x0);
+
+
     o.setStateKinematics(stateKine);
+    o.setGyroBias(Vector3(1,2,3));
+
+    Vector6 wrench;
+    wrench << 4,5,6,7,8,9;
+
+    o.setStateUnmodeledWrench(wrench);
+
+    
 
     Vector x = o.getStateVector();
     stateObservation::TimeIndex index = o.getStateVectorSampleTime();
+
+    Kinematics contactKine;
+    contactKine.position.set()<<0,0.1,0;
+    contactKine.orientation.setZeroRotation();
+
+
+
+    o.addContact(contactKine,0);
+
+    Matrix3 linStiffness,angStiffness,linDamping,angDamping;
+    linStiffness.setZero();
+    linStiffness.diagonal().setConstant(50000);
+
+    angStiffness.setZero();
+    angStiffness.diagonal().setConstant(400);
+
+    linDamping.setZero();
+    linDamping.diagonal().setConstant(500);
+
+    angDamping.setZero();
+    angDamping.diagonal().setConstant(20);
+    
+    contactKine.position.set()<<0,-0.1,0;
+    o.addContact(contactKine,linStiffness,linDamping,angStiffness,angDamping,3);
+
+    Matrix12 initialCov, processCov;
+
+    initialCov.setZero();
+    initialCov.diagonal().setConstant(0.01);
+    processCov.setZero();
+    processCov.diagonal().setConstant(0.0001);
+
+    contactKine.position.set()<< 1,0.1,0;
+    int i = o.addContact(contactKine,initialCov,processCov,-1);
+
+    assert(i==1);
+
+    contactKine.position.set() << 1,-0.1,0;
+    o.addContact(contactKine,initialCov,processCov,linDamping,
+                        linStiffness,angStiffness,angDamping,2);
+
+
+    
+
+
 
     std::cout << index << " " << x.transpose() << std::endl;
 
@@ -600,10 +659,97 @@ int testKineticsObserver(int code)
 
     std::cout << l;
 
+    std::cout << o.kineIndex() << " " <<
+     o.posIndex() << " " <<
+     o.oriIndex() << " " <<
+     o.linVelIndex() << " " <<
+     o.angVelIndex() << " " <<
+     o.gyroBiasIndex(0) << " " <<
+     o.gyroBiasIndex(1) << " " <<
+     o.unmodeledWrenchIndex() << " " <<
+     o.unmodeledForceIndex() << " " <<
+     o.unmodeledTorqueIndex() << " " <<
+     o.contactsIndex() << " " <<
+     o.contactIndex(0) << " " <<
+     o.contactKineIndex(0) << " " <<
+     o.contactPosIndex(0) << " " <<
+     o.contactOriIndex(0) << " " <<
+     o.contactForceIndex(0) << " " <<
+     o.contactTorqueIndex(0) << " " <<
+     o.contactWrenchIndex(0) << " " <<
+
+     o.contactIndex(1) << " " <<
+     o.contactKineIndex(1) << " " <<
+     o.contactPosIndex(1) << " " <<
+     o.contactOriIndex(1) << " " <<
+     o.contactForceIndex(1) << " " <<
+     o.contactTorqueIndex(1) << " " <<
+     o.contactWrenchIndex(1) << " " <<
+
+     o.contactIndex(2) << " " <<
+     o.contactKineIndex(2) << " " <<
+     o.contactPosIndex(2) << " " <<
+     o.contactOriIndex(2) << " " <<
+     o.contactForceIndex(2) << " " <<
+     o.contactTorqueIndex(2) << " " <<
+     o.contactWrenchIndex(2) << " " <<
+
+     o.contactIndex(3) << " " <<
+     o.contactKineIndex(3) << " " <<
+     o.contactPosIndex(3) << " " <<
+     o.contactOriIndex(3) << " " <<
+     o.contactForceIndex(3) << " " <<
+     o.contactTorqueIndex(3) << " " <<
+     o.contactWrenchIndex(3) << " " << std::endl;
     
+    
+    std::cout << o.kineIndexTangent() << " " <<
+     o.posIndexTangent() << " " <<
+     o.oriIndexTangent() << " " <<
+     o.linVelIndexTangent() << " " <<
+     o.angVelIndexTangent() << " " <<
+     o.gyroBiasIndexTangent(0) << " " <<
+     o.gyroBiasIndexTangent(1) << " " <<
+     o.unmodeledWrenchIndexTangent() << " " <<
+     o.unmodeledForceIndexTangent() << " " <<
+     o.unmodeledTorqueIndexTangent() << " " <<
+     o.contactsIndexTangent() << " " <<
 
+     o.contactIndexTangent( 0 ) << " " <<
+     o.contactKineIndexTangent( 0 ) << " " <<
+     o.contactPosIndexTangent( 0 ) << " " <<
+     o.contactOriIndexTangent( 0 ) << " " <<
+     o.contactForceIndexTangent( 0 ) << " " <<
+     o.contactTorqueIndexTangent( 0 ) << " " <<
+     o.contactWrenchIndexTangent( 0 ) << " " <<
+
+     o.contactIndexTangent( 1 ) << " " <<
+     o.contactKineIndexTangent( 1 ) << " " <<
+     o.contactPosIndexTangent( 1 ) << " " <<
+     o.contactOriIndexTangent( 1 ) << " " <<
+     o.contactForceIndexTangent( 1 ) << " " <<
+     o.contactTorqueIndexTangent( 1 ) << " " <<
+     o.contactWrenchIndexTangent( 1 ) << " " <<
      
+     o.contactIndexTangent( 2 ) << " " <<
+     o.contactKineIndexTangent( 2 ) << " " <<
+     o.contactPosIndexTangent( 2 ) << " " <<
+     o.contactOriIndexTangent( 2 ) << " " <<
+     o.contactForceIndexTangent( 2 ) << " " <<
+     o.contactTorqueIndexTangent( 2 ) << " " <<
+     o.contactWrenchIndexTangent( 2 ) << " " <<
 
+     o.contactIndexTangent( 3 ) << " " <<
+     o.contactKineIndexTangent( 3 ) << " " <<
+     o.contactPosIndexTangent( 3 ) << " " <<
+     o.contactOriIndexTangent( 3 ) << " " <<
+     o.contactForceIndexTangent( 3 ) << " " <<
+     o.contactTorqueIndexTangent( 3 ) << " " <<
+     o.contactWrenchIndexTangent( 3 ) << " " << std::endl;
+     
+    o.setWithUnmodeledWrench(true );
+    o.setWithAccelerationEstimation(true );
+    o.setWithGyroBias(true);
 
     return 0;
 }
@@ -634,7 +780,7 @@ int main()
         std::cout << "Kinematics test succeeded" << std::endl;
     }
 
-    if ((returnVal = testKineticsObserver(3)))
+    if ((returnVal = testKineticsObserverCodeAccessor(3)))
     {
         std::cout<< "Kinetics Observer test failed, code : 3" <<std::endl;
         return returnVal;
