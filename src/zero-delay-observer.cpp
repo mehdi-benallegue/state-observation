6c94b28d49d6fd5a37bda04ddd36c458990c1727
Mehdi Benallegue <mehdi.benallegue@aist.go.jp>
2020-11-09 20:48:22 +0900

    [core] improve documentation

--- src/zero-delay-observer.cpp
+++ src/zero-delay-observer.cpp
@@ -112,7 +112,7 @@ void ZeroDelayObserver::clearInputs()
   if(p_ > 0) u_.reset();
 }
 
-ObserverBase::StateVector ZeroDelayObserver::getEstimatedState(TimeIndex k)
+ObserverBase::StateVector ZeroDelayObserver::estimateState(TimeIndex k)
 {
   BOOST_ASSERT(x_.isSet() && "The state vector has not been set");
 
