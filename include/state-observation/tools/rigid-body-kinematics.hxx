e3f06f0f91606fe271a09501c1dcf11727c3c321
Mehdi Benallegue <mehdi.benallegue@aist.go.jp>
2021-03-12 21:37:40 +0900

    [core] move to local frame of the invariant vector

--- include/state-observation/tools/rigid-body-kinematics.hxx
+++ include/state-observation/tools/rigid-body-kinematics.hxx
@@ -406,7 +406,7 @@ inline Matrix3 mergeRoll1Pitch1WithYaw2(const Matrix3 & R1, const Matrix3 & R2, 
 
 inline Matrix3 mergeTiltWithYawAxisAgnostic(const Vector3 & Rtez, const Matrix3 & R2)
 {
-  return mergeTiltWithYaw(Rtez, R2, getInvariantHorizontalVector(R2).normalized());
+  return mergeTiltWithYaw(Rtez, R2, getInvariantHorizontalVector(R2.transpose()).normalized());
 }
 
 inline Matrix3 mergeRoll1Pitch1WithYaw2AxisAgnostic(const Matrix3 & R1, const Matrix3 & R2)
